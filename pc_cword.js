7cfec5e3fc8c1c1e7ee3e24933ad505fbf6d3dcc
Chris Kim <ckim990@west-mec.org>
2019-04-09 21:46:36 -0700

    JS Complete

--- pc_cword.js
+++ pc_cword.js
@@ -119,3 +119,89 @@ function init() {
 function getChar(keyNum) {
    return String.fromCharCode(keyNum);
 }
+
+function formatPuzzle(puzzleLetter) {
+   currentLetter = puzzleLetter;
+
+   for (var i = 0; i < allLetters.length; i++) {
+      allLetters[i].style.backgroundColor = "";
+   }
+
+   acrossClue.style.color = "rgb(96, 96, 28)";
+   downClue.style.color = "rgb(96, 96, 28)";
+
+   if (currentLetter.dataset.clueA != undefined) {
+      acrossClue = document.getElementById(currentLetter.dataset.clueA);
+      acrossClue.style.color = "blue";
+      wordLetters = document.querySelectorAll('[data-clue-a =' + currentLetter.getAttribute('data-clue-a') + ']');
+      for (var i = 0; i < wordLetters.length; i++) {
+         wordLetters[i].style.backgroundColor = "rgb(231, 231, 255)";
+      }
+   }
+
+   if (currentLetter.dataset.clueD != undefined) {
+      downClue = document.getElementById(currentLetter.dataset.clueD);
+      downClue.style.color = "red";
+      wordLetters = document.querySelectorAll("[data-clue-d =" + currentLetter.getAttribute('data-clue-d') + "]");
+      for (var i = 0; i < wordLetters.length; i++) {
+         wordLetters[i].style.backgroundColor = "rgb(255, 231, 231)";
+      }
+   }
+
+   if (typeDirection === "right") {
+      currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
+   } else {
+      currentLetter.style.backgroundColor = "rgb(255, 231, 231)";
+   }
+
+
+}
+
+function selectLetter() {
+   var leftLetter = document.getElementById(currentLetter.dataset.left);
+   var upLetter = document.getElementById(currentLetter.dataset.up);
+   var rightLetter = document.getElementById(currentLetter.dataset.right);
+   var downLetter = document.getElementById(currentLetter.dataset.down);
+   var userKey = event.keyCode;
+
+   if (userKey === 37) {
+      formatPuzzle(leftLetter);
+   } else if (userKey === 38) {
+      formatPuzzle(upLetter);
+   } else if (userKey === 39 || userKey === 9) {
+      formatPuzzle(rightLetter);
+   } else if (userKey === 40 || userKey === 13) {
+      formatPuzzle(downLetter);
+   } else if (userKey === 8 || userKey === 46) {
+      currentLetter.textContent = "";
+   } else if (userKey === 32) {
+      switchTypeDirection();
+   } else if (userKey >= 65 && userKey <= 90) {
+      currentLetter.textContent = getChar(userKey)
+      if (typeDirection === "right") {
+         formatPuzzle(rightLetter);
+      } else {
+         formatPuzzle(downLetter);
+      }
+
+   }
+   event.preventDefault();
+}
+
+function switchTypeDirection() {
+
+   var typeImage = document.getElementById('directionImg');
+   if (typeDirection === "right") {
+      typeDirection = "down";
+      typeImage.src = "pc_right.png";
+      currentLetter.style.backgroundColor = "rgb(255,191, 191)";
+   } else {
+      typeDirection = "right";
+      typeImage.src = "pc_down.png";
+      currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
+   }
+}
+
+function getChar(keyNum) {
+   return String.fromCharCode(keyNum);
+}
